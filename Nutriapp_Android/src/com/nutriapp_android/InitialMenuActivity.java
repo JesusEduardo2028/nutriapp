44eb273f505f51f89e46783ab29c283bbeead1f6
franknavia <naviaurbano@unicauca.edu.co>
2013-10-29 15:18:08 -0500

    agregada clase InitialMenu para dirigir la aplicacion a traves de una dieta personalizada o busqueda de recetas

---
+++ Nutriapp_Android/src/com/nutriapp_android/InitialMenuActivity.java
@@ -0,0 +1,48 @@
+package com.nutriapp_android;
+
+import com.nutriapp_android.internal_storage.SharedPreferencesHelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class InitialMenuActivity extends Activity {
+	CheckBox config_inicial;
+	
+	SharedPreferencesHelper shared_preference;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_initial_menu);
+		
+		config_inicial = (CheckBox) findViewById(R.id.checkBoxConfigInicial);
+		
+		shared_preference = new SharedPreferencesHelper(this);
+	}
+	
+	public void dietaPersonalizada(View v) {
+		if(config_inicial.isChecked()) {
+			shared_preference.writeStringShared("configuracion_inicial", "true");
+			shared_preference.writeStringShared("configuracion_tipo", "dieta_personalizada");
+		}
+		
+		Intent registerIntent = new Intent(this, LoginActivity.class);
+    	startActivity(registerIntent);
+    	finish();
+	}
+	
+	public void botonRecetas(View v) {		
+		if(config_inicial.isChecked()) {
+			shared_preference.writeStringShared("configuracion_inicial", "true");
+			shared_preference.writeStringShared("configuracion_tipo", "buscar_receta");
+		}
+		
+		/*Intent registerIntent = new Intent(this, SearchRecipesActivity.class);
+    	startActivity(registerIntent);
+    	finish();*/
+	}
+
+}
